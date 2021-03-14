91bc6708347a7ec932e6f18ee63ff08cf8da628e
Giao Ho <joutvhu@gmail.com>
2021-03-14 13:00:32 +0700

    Allow get parent dirname from another file path, add comment

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import {dirname, resolve} from 'path';
 
-export function parentDirname(): string {
+/**
+ * Get parent directory path
+ *
+ * @param filePath you need get parent directory path
+ */
+export function parentDirname(filePath: string = __filename): string {
     const _pst = Error.prepareStackTrace;
     Error.prepareStackTrace = (_, cs) => cs;
     const stacks: NodeJS.CallSite[] = (new Error() as any).stack;
@@ -11,15 +16,22 @@ export function parentDirname(): string {
         .map(value => value.getFileName())
         .filter(value => typeof value === 'string' && !value.startsWith('internal/'))
         .find(value => {
-            if (value === __filename)
+            if (value === filePath)
                 seenThis = true;
-            else if (seenThis && value !== __filename)
+            else if (seenThis && value !== filePath)
                 return true;
             return false;
         });
-    return path != null ? dirname(path) : __dirname;
+    return dirname(path != null ? path : filePath);
 }
 
+/**
+ * Import by a string reference
+ *
+ * @param ref string reference, format: "<file path>#<property path, split by dot>"
+ * @param parentPath is an absolute path to resolve file path
+ * @param inner use default if property path does not specified
+ */
 export function importBy<T>(ref: string, parentPath: string = parentDirname(), inner = true): T | any {
     let [path, name] = ref.split('#', 2);
 
